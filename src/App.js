55386743c5ab4e1ede803b52bca8f2b323c6faf6
bigmosi <kinyeramo@gmail.com>
2023-06-06 09:59:02 +0300

    added github workflows

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import RestaurantList from './components/RestaurantList';
+import RestaurantCreate from './components/RestaurantCreate';
+import RestaurantDetails from './components/RestaurantDetails';
+import RestaurantUpdate from './components/RestaurantUpdate';
+import RestaurantDelete from './components/RestaurantDelete';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={RestaurantList} />
+          <Route exact path="/restaurants/create" component={RestaurantCreate} />
+          <Route exact path="/restaurants/:id" component={RestaurantDetails} />
+          <Route exact path="/restaurants/:id/edit" component={RestaurantUpdate} />
+          <Route exact path="/restaurants/:id/delete" component={RestaurantDelete} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
